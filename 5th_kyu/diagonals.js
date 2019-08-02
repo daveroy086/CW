47fabc2698182a1c5e602c1f4951de01f5efc2f5
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2019-08-02 05:50:32 -0400

    yuk

--- 5th_kyu/diagonals.js
+++ 5th_kyu/diagonals.js
@@ -1,5 +1,29 @@
 function diagonals(matrix) {
-  return matrix;
+    var returnMatrix = [];
+    var resultMatrix = [];
+    len = matrix.length;
+
+    //will return the upper, left to lower, right diagonals from h largest down to the lower left corner 
+    for (let i = 0; i < len; i++){
+        for (let j = 0; j < len - i; j++){
+            resultMatrix.push(matrix[i][i + 1]);
+            console.log('i is ' + i + ', j is ' + j + ' and matrix[i][i + j] is ' + matrix[i][i + j]);
+            console.log(resultMatrix);
+            }//end for j
+        }//end for i
+    returnMatrix.push(resultMatrix);
+    console.log(returnMatrix);
+
+    //will retrun the upper,left to lower, right diagonals up to the upper right corner
+     //create for (i = 1; i < len; i++){
+    //  create for (j = 0; j < len - i; j++){
+    //get the matrix element at i,j...resultMatrix.push(matrix[i][j])
+    //              }//end for j
+    //          }//end for i
+    //returnMatrix.push(resultMatrix);
+
+
+  //return matrix;
 }
 
 /*function sort(array) {
@@ -19,4 +43,4 @@ function diagonals(matrix) {
 }
 */
 
-//diagonals([[1,2,3], [4,5,6], [7,8,9]]))// [[1],[1,5,9],[2,4],[2,6],[3],[3,5,7],[4,8],[6,8],[7],[9]]);
+diagonals([[1,2,3], [4,5,6], [7,8,9]])// [[1],[1,5,9],[2,4],[2,6],[3],[3,5,7],[4,8],[6,8],[7],[9]]);
