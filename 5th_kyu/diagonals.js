9b315a0bfc9faeb7f58098bb4fcf14fcec38e3e2
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2019-08-03 09:13:23 -0400

    works

--- 5th_kyu/diagonals.js
+++ 5th_kyu/diagonals.js
@@ -11,10 +11,10 @@ function diagonals(matrix) {
           }else{
             aResultMatrix.push(matrix[j][k]);
           }
-            }//end for j
+        }//end for j
             returnMatrix.push(aResultMatrix);
-            console.log(returnMatrix);
-        }//end for i
+    }//end for i
+
         //will return the upper, left to lower, right diagonals from the largest down up to the upper right corner 
     for (let m = 0; m < len - 1; m++){
          var bResultMatrix = []; 
@@ -23,41 +23,13 @@ function diagonals(matrix) {
           if(p < 0){
           }else{
             bResultMatrix.push(matrix[n][p]);
-            console.log(bResultMatrix);
           }
-            }//end for j
+        }//end for j
             returnMatrix.push(bResultMatrix);
-        }//end for i
+    }//end for i
     
     console.log(returnMatrix);
-
-    //will retrun the upper,left to lower, right diagonals up to the upper right corner
-     //create for (i = 1; i < len; i++){
-    //  create for (j = 0; j < len - i; j++){
-    //get the matrix element at i,j...resultMatrix.push(matrix[i][j])
-    //              }//end for j
-    //          }//end for i
-    //returnMatrix.push(resultMatrix);
-
-
-  //return matrix;
-}
-
-/*function sort(array) {
-  return array.slice().sort(function(arr1, arr2) {
-    var i;
-    for (i = 0; i < arr1.length && i < arr2.length && arr1[i] === arr2[i]; i++);
-    if (i < arr1.length && i < arr2.length) {
-      return arr1[i] - arr2[i];
-    } else if (i === arr1.length) {
-      return -1;
-    } else if (i === arr2.length) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
-}
-*/
+}//end diagonals
 
 diagonals([[1,2,3], [4,5,6], [7,8,9]])// [[1],[1,5,9],[2,4],[2,6],[3],[3,5,7],[4,8],[6,8],[7],[9]]);
+//diagonals([[1,2,3,4], [5,6,7,8],[9,0,11,12],[13,14,15,16]]);
